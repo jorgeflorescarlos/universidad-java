cdac326eb1de17755a707dffaa25911ae045639a
Jorge Flores Carlos <al221510568@gmail.com>
2021-02-02 21:38:43 -0600

    6. Variables en Java

--- fundamentos/01-hola-mundo/src/HolaMundo.java
+++ fundamentos/01-hola-mundo/src/HolaMundo.java
@@ -5,6 +5,7 @@
  */
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java");
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
     }
 }
